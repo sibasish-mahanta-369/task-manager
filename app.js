70baf479b55a7383fe64df28415f313626e58012
sibasish-mahanta-369 <sibasishmahanta27032001@gmail.com>
2022-07-12 04:23:32 -0400

    added routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks =  require('./routes/tasks')
+const port = 3000;
+
+app.use(express.json())
+
+app.get('/home',(req,res)=>{
+    res.send('home page');
+})
+
+app.use('/api/v1/tasks',tasks)
+
+app.listen(port,console.log(`server is listeninbg on port ${port}...`))
