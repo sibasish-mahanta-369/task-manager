39ee1aabc1e5a26137e355fdeefda7bc102c959f
sibasish-mahanta-369 <sibasishmahanta27032001@gmail.com>
2022-07-14 11:17:20 -0400

    modified task controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,4 +2,20 @@ const getAllTasks = (req,res)=>{
     res.send('all items of the list')
 }
 
+const createTask = (req,res)=>{
+    res.json(req.body)
+}
+
+const getTask = (req,res)=>{
+    res.json({id:req.params.id})
+}
+
+const updateTask = (req,res)=>{
+    res.send('single task updated')
+}
+
+const deleteTask = (req,res)=>{
+    res.send('single task deleted')
+}
+
 module.exports = {getAllTasks, createTask, getTask,updateTask,deleteTask}
