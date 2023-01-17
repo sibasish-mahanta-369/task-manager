005eb09d19c123fe2828ecc2000d2b59a924d77e
sibasish-mahanta-369 <sibasishmahanta27032001@gmail.com>
2023-01-17 18:51:45 +0530

    connected to database

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB =  require('./db/connect')
+require('dotenv').config()
+
+app.get('/',(req,res)=>{
+    res.send('home page')
+})
+
+app.use(express.json())
+
+app.use('/api/v1/tasks',tasks)
+
+const port = 3000
+
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,()=>{
+            console.log(`Listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
